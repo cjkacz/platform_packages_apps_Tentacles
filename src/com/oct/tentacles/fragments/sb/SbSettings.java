74d2c017ff7aa17c1a9e805994fb8ef94e0254c1
Griffin Millender <griffinn.millender@gmail.com>
2014-03-18 08:37:40 -0400

    Tentacles: add Battery Styles the new way 2/2 google draws now the battery icon which fits perfectly into our concept. Beside that the batteryview is as well used on QS. So we readd our battery icons (icon, percentage, icon with percentage circle, circle perc, dot circle, dot circle perc) back and all are drawn. As well the icon does not only change on statusbar as well on QS it is reflected. As usual color options are present for the User to style the icon
    Change-Id: Iaedede55e14d99f79e560e74188692c894f2d961
    
    Conflicts:
    	res/values/strings.xml
    	src/com/oct/tentacles/fragments/sb/SbSettings.java

--- src/com/oct/tentacles/fragments/sb/SbSettings.java
+++ src/com/oct/tentacles/fragments/sb/SbSettings.java
@@ -90,8 +90,9 @@ public class SbSettings extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new NotificationDrawer();
-            frags[1] = new QuickSettings();
+			frags[0] = new BatterySettings();
+            frags[1] = new NotificationDrawer();
+			frags[2] = new QuickSettings();
         }
 
         @Override
@@ -113,7 +114,8 @@ public class SbSettings extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-                    getString(R.string.sb_notification_category),
+		            getString(R.string.sb_battery_category),
+					getString(R.string.sb_notification_category),
                     getString(R.string.sb_quicksettings_category)};
         return titleString;
     }
