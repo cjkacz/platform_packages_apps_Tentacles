6bf887740c87aaae6474ae7ac1b6edb5887e8cb3
treken <chupekevin@gmail.com>
2014-05-05 08:59:36 -0400

    New Theme engine: Add sliding tab and remove tmo entry.

--- src/com/oct/tentacles/fragments/ThemerSettings.java
+++ src/com/oct/tentacles/fragments/ThemerSettings.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2014 Team OctOS
- * Warning: Tentacles may strangle!
+ * Copyright (C) 2014 OctOS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,19 +16,103 @@
 
 package com.oct.tentacles.fragments;
 
-import android.content.ActivityNotFoundException;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.content.Context;
 import android.os.Bundle;
-import android.preference.Preference;
-import com.android.settings.ActivityPicker;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.oct.tentacles.R;
 import com.oct.tentacles.preference.SettingsPreferenceFragment;
+import com.oct.tentacles.Utils;
+
+import com.oct.tentacles.fragments.*;
+
+import java.lang.Exception;
+import java.util.ArrayList;
 
 public class ThemerSettings extends SettingsPreferenceFragment {
 
+    private static final String TAG = "ThemerSettings";
+
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    String titleString[];
+
+    ViewGroup mContainer;
+
+    static Bundle mSavedState;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.themer_settings);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.tab_ui_sb, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+        mPagerTabStrip.setTabIndicatorColorResource(android.R.color.white);
+
+        StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
+        mViewPager.setAdapter(StatusBarAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!Utils.isTablet(getActivity())) {
+            mContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    class StatusBarAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public StatusBarAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new ThemeSettings();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                    getString(R.string.themer_settings_title)};
+        return titleString;
     }
 }
